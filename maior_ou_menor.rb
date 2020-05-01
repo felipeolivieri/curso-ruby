9a86ea67ee954ba2ec45a43a8304621e0c8b4f19
felipeolivieri <felipe.olivieri@gmail.com>
2020-05-01 10:11:38 -0300

    segunda versao, nesta versao fiz uma limpeza no codigo e reorganizacao tirando comentarios desnecessarios e incluindo comentario no inicio do arquivo para explicar o que faz o codigo

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,32 +1,25 @@
+=begin
+Este é meu primeiro programa em ruby. estou aprendendo a desenvolver e este é o primeiro exercicio sugerido no curso online da Alura.
+
+O que faz? É um jogo de adivinhação de numero, podemos chutar um numero de 0 a 200 e temos uma quantilidade limitada para adivinhar.
+
+=end
+
+numero_secreto = 175
 puts("Bem vindo ao jogo de adivinhação")
 puts "Qual é o seu nome?"
 nome = gets
 puts "\n\n"
 puts "Começaremos o jogo para você, " + nome
 puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 puts "\n\n\n\n\n"
-
-# tentativas infinitas
-#tentativa = 0
-#while true	
-
-=begin
-	Multiplas linhas de comentarios podem
-	ser feitas assim. fica muito feio
-	mas funciona.
-	para escrever grandes textos, explicando algo
-	pode ser util. melhor que ficar colocando tralha
-	em toda linha.
-=end
 
 # quantidade de tentativas
 limite_de_tentativas = 5
 puts "Você terá " + limite_de_tentativas.to_s + " tentativas! Boa Sorte!"
-# apenas n tentativas
+
 for tentativa in 1..limite_de_tentativas
-	#tentativa += 1
 	puts "Tentativa : #{tentativa} de " + limite_de_tentativas.to_s
 	puts "Entre com o número (<200)"
 	chute = gets
@@ -46,7 +39,3 @@ for tentativa in 1..limite_de_tentativas
 		end
 	end
 end
-
-
-
-
