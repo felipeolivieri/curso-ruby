b144dd63f9f7a9d1c0274d4325bf6d6f015929c9
Felipe Olivieri <felipe.olivieri@gmail.com>
2020-05-08 14:28:22 -0300

    organização de código - defini funcoes e alterei como chamar os argumentos

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -46,6 +46,7 @@ end
 da_boas_vindas
 numero_secreto = sorteia_numero_secreto
 
+pontos_ate_agora = 1000
 limite_de_tentativas = 5
 chutes = []
 
@@ -56,7 +57,12 @@ for tentativa in 1..limite_de_tentativas
 	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
 	chutes << chute
 	
+	pontos_a_perder = (chute - numero_secreto).abs/2.0
+	pontos_ate_agora -= pontos_a_perder
+
 	if verifica_se_acertou numero_secreto, chute, tentativa
 		break
 	end
 end
+
+puts "Você ganhou #{pontos_ate_agora} pontos!"
