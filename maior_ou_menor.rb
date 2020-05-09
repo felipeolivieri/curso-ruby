eea98f96e76ea0aeab9b5cfbd609679623d45fdf
Felipe Olivieri <felipe.olivieri@gmail.com>
2020-05-09 09:49:23 -0300

    implementando funcao que pede dificuldade do jogo(1 a 5)

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -12,11 +12,33 @@ def da_boas_vindas
   
 end
 
-def sorteia_numero_secreto
-  puts "Escolhendo um número secreto entre 0 e 200..."
-  sorteado = rand(200)
-  puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-  sorteado
+def pede_dificuldade
+	puts "Qual o nivel de dificuldade que deseja? (1 fácil, 5 difícil)"
+	dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto(dificuldade)
+	if dificuldade == 1
+		maximo = 30
+	else
+		if dificuldade == 2
+			maximo = 60
+		else
+			if dificuldade == 3
+				maximo = 100
+			else
+				if dificuldade ==4
+					maximo = 150
+				else
+					maximo = 200
+				end
+			end
+		end
+	end			
+ 	puts "Escolhendo um número secreto entre 0 e #{maximo -1}..."
+  	sorteado = rand(maximo)
+  	puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+  	sorteado
 end  
 
 def pede_um_numero(chutes, tentativa, limite_de_tentativas)
@@ -44,7 +66,8 @@ def verifica_se_acertou(numero_secreto, chute, tentativa)
 end
 
 da_boas_vindas
-numero_secreto = sorteia_numero_secreto
+dificuldade = pede_dificuldade
+numero_secreto = sorteia_numero_secreto(dificuldade)
 
 pontos_ate_agora = 1000
 limite_de_tentativas = 5
